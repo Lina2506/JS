00ef771de1d03eb3c46b4da8c18315a514634d83
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 16:40:10 +0200

    add switch

---
+++ LESSON2/switch/main.js
@@ -0,0 +1,39 @@
+switch ('Monday') {
+    case 'Monday':
+        console.log('monday');
+        console.log('monday');
+        console.log('monday');
+        break;
+    case 'Tuesday':
+        console.log('tuesday');
+        console.log('tuesday');
+        console.log('tuesday');
+        break;
+    case 'Wednesday':
+        console.log('Wednesday');
+        console.log('Wednesday');
+        console.log('Wednesday');
+        break;
+    case 'Thursday':
+        console.log('Thursday');
+        console.log('Thursday');
+        console.log('Thursday');
+        break;
+    case 'Friday':
+        console.log('Friday');
+        console.log('Friday');
+        console.log('Friday');
+        break;
+    case 'Saturday':
+        console.log('Saturday');
+        console.log('Saturday');
+        console.log('Saturday');
+        break;
+    case 'Sunday':
+        console.log('Sunday');
+        console.log('Sunday');
+        console.log('Sunday');
+        break;
+    default:
+        console.log('???')
+}
