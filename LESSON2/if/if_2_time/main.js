263b35a895f14d071982ad2ab1b6e46dfb2cc7e4
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 13:20:41 +0200

    add if_2_time

---
+++ LESSON2/if/if_2_time/main.js
@@ -0,0 +1,12 @@
+let time=+prompt('what time is it?');
+if (time<=15){
+    console.log('first');
+}else if (time<=30){
+    console.log('second');
+}else if (time<=45){
+    console.log('third');
+}else if (time<=59){
+    console.log('fourth');
+}else{
+    console.log('???');
+}
