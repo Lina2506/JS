b6c295fefca90ca4c6554bf0f3bca857c251983f
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 11:43:55 +0200

    add weather

---
+++ LESSON2/weather/main.js
@@ -0,0 +1,42 @@
+let Monday={
+    morning:7,
+    day:15,
+    evening:10
+}
+let Tuesday={
+    morning:5,
+    day:12,
+    evening:7
+}
+let Wednesday={
+    morning:7,
+    day:15,
+    evening:10
+}
+let Thursday={
+    morning:7,
+    day:11,
+    evening:12
+}
+let Friday={
+    morning:8,
+    day:19,
+    evening:9
+}
+let Saturday={
+    morning:4,
+    day:13,
+    evening:10
+}
+let Sunday={
+    morning:6,
+    day:16,
+    evening:14
+}
+console.log(Monday['day'])
+console.log(Tuesday['day'])
+console.log(Wednesday['day'])
+console.log(Thursday['day'])
+console.log(Friday['day'])
+console.log(Saturday['day'])
+console.log(Sunday['day'])
