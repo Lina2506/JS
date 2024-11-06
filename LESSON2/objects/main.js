7fa55729f5891977273e8f03c3170fc84b2a258c
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 10:03:24 +0200

    add objects

---
+++ LESSON2/objects/main.js
@@ -0,0 +1,15 @@
+let book1={
+    title: "Dawn in the mountains",
+    pageCount: 326,
+    genre: 'horror'
+}
+let book2={
+    title: "Rainbow",
+    pageCount: 12,
+    genre: 'tale'
+}
+let book3={
+    title: "Mystery",
+    pageCount: 980,
+    genre: 'drama'
+}
