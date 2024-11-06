8246d8d18fce75cb8662f56728120ff251517357
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 13:25:55 +0200

    add if_3_day

---
+++ LESSON2/if/if_3_day/main.js
@@ -0,0 +1,10 @@
+let day=+prompt('what day is now?');
+if (day<=10){
+    console.log('first');
+}else if (day<=20){
+    console.log('second');
+}else if (day<=31){
+    console.log('third');
+}else{
+    console.log('???');
+}
