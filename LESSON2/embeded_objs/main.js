118a636d2fbd6811a025bf2f870da4ee557283b9
Lina2506 <allarenotme.lina@gmail.com>
2024-11-06 11:09:39 +0200

    add objects_user

---
+++ LESSON2/embeded_objs/main.js
@@ -0,0 +1,28 @@
+let book1={
+    title: "Dawn in the mountains",
+    pageCount: 326,
+    genre: 'horror',
+    authors: {
+        name: 'Luiza Washington',
+        age: 34
+    }
+}
+let book2={
+    title: "Rainbow",
+    pageCount: 12,
+    genre: 'tale',
+    authors: {
+        name: 'David Jonson',
+        age: 69
+    }
+}
+let book3={
+    title: "Mystery",
+    pageCount: 980,
+    genre: 'drama',
+    authors: {
+        name: 'Adam Miller',
+        age: 42
+    }
+}
+console.log(book1,book2,book3)
---
+++ LESSON2/objects_user/main.js
@@ -0,0 +1,60 @@
+let user1={
+    name: 'Vitalina',
+    username:'vita',
+    password: 7638273984
+}
+let user2={
+    name: 'Paul',
+    username:'paulozzy',
+    password: '83290Pjh89'
+}
+let user3={
+    name: 'Svitlana',
+    username:'lana',
+    password: 384283298
+}
+let user4={
+    name: 'Margarita',
+    username:'rita',
+    password: 6398742980928
+}
+let user5={
+    name: 'Anna',
+    username:'neuta',
+    password: 'dhfh379879'
+}
+let user6={
+    name: 'John',
+    username:'jonny',
+    password: 'jkhf9987'
+}
+let user7={
+    name: 'David',
+    username:'dave',
+    password: 9348193029
+}
+let user8={
+    name: 'Mishel',
+    username:'mich',
+    password: 'jdhf876876'
+}
+let user9={
+    name: 'Sandra',
+    username:'sun',
+    password: 'jkshd6768'
+}
+let user10={
+    name: 'Makar',
+    username:'makarena',
+    password: 93874893203
+}
+console.log(user1.password)
+console.log(user2.password)
+console.log(user3.password)
+console.log(user4.password)
+console.log(user5.password)
+console.log(user6.password)
+console.log(user7.password)
+console.log(user8.password)
+console.log(user9.password)
+console.log(user10.password)
