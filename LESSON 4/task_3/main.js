fcaff5d9411bb74571286df0c33a80020580fe87
Lina2506 <allarenotme.lina@gmail.com>
2024-11-11 12:46:23 +0200

    add task_3, task_5

---
+++ LESSON 4/task_3/main.js
@@ -0,0 +1,7 @@
+function cylinder(radius,height) {
+    const result = 2 * Math.PI * radius * height;
+    console.log(result);
+    return result;
+}
+
+cylinder(15, 40);
---
+++ LESSON 4/task_5/main.js
@@ -0,0 +1,8 @@
+function foobar(text) {
+    document.write(`<p>${text}</p>`);
+
+}
+
+foobar('hello');
+foobar('it´s');
+foobar('me');
