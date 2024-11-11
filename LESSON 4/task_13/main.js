39abc65244953e8035ff2ab7022ac3623456ca68
Lina2506 <allarenotme.lina@gmail.com>
2024-11-11 16:38:16 +0200

    add task_13

--- LESSON 4/task_13/main.js
+++ LESSON 4/task_13/main.js
@@ -5,8 +5,9 @@ function exchange(sumUAH,currencyValues,exchangeCurrency) {
             chosenCurrency = item;
         }
     }
-    let result = sumUAH / chosenCurrency.value;
-    return result;
+    let count = sumUAH / chosenCurrency.value;
+    return count;
 }
+
 
 console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
