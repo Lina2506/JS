275b8b3acdcf8425c1dc8df182d5393aa87435f8
Lina2506 <allarenotme.lina@gmail.com>
2024-11-12 21:13:28 +0200

    add task_3 for LESSON5

--- LESSON 5/task_3/main.js
+++ LESSON 5/task_3/main.js
@@ -1,2 +1,3 @@
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 let cylinder = (radius, height) => 2 * Math.PI * radius * height;
 console.log(cylinder(15, 40));
