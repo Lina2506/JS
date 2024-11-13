524ecb4507068355528c54ef68277da558b12a90
Lina2506 <allarenotme.lina@gmail.com>
2024-11-13 08:07:50 +0200

    add task_5 for LESSON5

---
+++ LESSON 5/task_5/main.js
@@ -0,0 +1,5 @@
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+let foobar=(text)=>document.write(`<p>${text}</p>`);
+foobar('hello');
+foobar('it´s');
+foobar('me');
