12a7e2894c610d69a53e7d28ca2c9b575e49f20c
Lina2506 <allarenotme.lina@gmail.com>
2024-11-14 10:00:20 +0200

    add task_9 for LESSON5

---
+++ LESSON 5/task_9/main.js
@@ -0,0 +1,19 @@
+let users = [
+    {id: 1, name: 'vasya', age: 31},
+    {id: 2, name: 'petya', age: 30},
+    {id: 3, name: 'kolya', age: 29},
+    {id: 4, name: 'olya', age: 28},
+    {id: 5, name: 'max', age: 30},
+    {id: 6, name: 'anya', age: 31},
+    {id: 7, name: 'oleg', age: 28},
+    {id: 8, name: 'andrey', age: 29},
+    {id: 9, name: 'masha', age: 30},
+    {id: 10, name: 'olya', age: 31},
+    {id: 11, name: 'max', age: 31},
+];
+let foobar = () => {
+    for (const user of users) {
+        document.write(`<div>${user.id} ${user.name} ${user.age}</div>`)
+    }
+};
+foobar();
