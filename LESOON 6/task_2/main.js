0ae03bc0d30d39e9a4f8413baff0f299340a1354
Lina2506 <allarenotme.lina@gmail.com>
2024-11-15 17:41:52 +0200

    add task_2 for LESSON6

---
+++ LESOON 6/task_2/main.js
@@ -0,0 +1,9 @@
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+let s1 = 'hello world';
+console.log(s1.toUpperCase());
+let s2 = 'lorem ipsum';
+console.log(s2.toUpperCase());
+let s = 'javascript is cool';
+const s3 = s.toUpperCase();
+console.log(s3);
