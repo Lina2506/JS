fa3b9143404ef0322ebafa1055a0070188404b50
Lina2506 <allarenotme.lina@gmail.com>
2024-11-22 18:32:33 +0200

    add task_4 for LESSON9

---
+++ LESSON9/task_4/main.js
@@ -0,0 +1,35 @@
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+for (const course of coursesAndDurationArray) {
+    const item = document.createElement('div');
+    const heading = document.createElement('h1');
+    heading.innerText = course.title;
+    const description = document.createElement('p');
+    description.innerText = course.monthDuration;
+
+    document.body.appendChild(heading);
+    document.body.appendChild(description);
+
+
+    document.body.appendChild(item);
+
+}
