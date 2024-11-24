873fdd5a1567a5a0be57c729d81f7acc8de3cdec
Lina2506 <allarenotme.lina@gmail.com>
2024-11-24 14:42:02 +0200

    add task_1 for LESSON10

---
+++ LESSON10/task_1/main.js
@@ -0,0 +1,8 @@
+// #sH8c4er
+// - Створити довільний елемент з id = text та створити кнопку.
+// Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+
+const button = document.getElementById('btn');
+button.onclick = function () {
+    document.getElementById('text').remove();
+};
